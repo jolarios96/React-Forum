6b8fa10181014ef4ace41ed8195ad47cda4f85d8
Jordan Larios <jolarios96@gmail.com>
2022-07-09 15:32:55 -0500

    added footer and header components

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    state = {  } 
+    render() { 
+        return <h1>This is the Footer!</h1>;
+    }
+}
+ 
+export default Footer;
+ 
