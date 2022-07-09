d99be810049810b720d743886ae2bb39082408fe
Jordan Larios <jolarios96@gmail.com>
2022-07-09 15:38:56 -0500

    temp header/footer

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+import App from "./components/app";
+import Header from "./components/header";
+import Footer from "./components/footer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Header />
     <App />
+    <Footer />
   </React.StrictMode>
 );
 
