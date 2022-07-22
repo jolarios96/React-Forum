103bb1a79fa25f90677d5abe0bd34fbe682ce15a
Jordan Larios <jolarios96@gmail.com>
2022-07-22 11:13:43 -0500

    update mysql config

--- server/config.js
+++ server/config.js
@@ -1,1 +1,6 @@
-require(".env").config();
+require("dotenv").config();
+
+var host = process.env.host;
+var user = process.env.user;
+var password = process.env.password;
+var database = process.env.db;
