679a1654525c220026a56d4ddaba25277a8bd396
Jordan Larios <jolarios96@gmail.com>
2022-07-22 11:14:09 -0500

    update server.js with mysql connection

--- server/server.js
+++ server/server.js
@@ -1,13 +1,34 @@
 var express = require("express");
 var cors = require("cors");
-var app = express();
+var config = require("./config.js");
 
+var app = express();
 app.use(cors());
+
+const mysql = require("mysql");
+console.log(config.host);
+
+const connection = mysql.createConnection({
+  host: config.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.db,
+});
+
+connection.connect();
+
+// connection.query("SELECT 1 + 1 AS solution", (err, rows, fields) => {
+//   if (err) throw err;
+
+//   console.log("The solution is: ", rows[0].solution);
+// });
+
+connection.end();
 
 app.get("/products/:id", function (req, res, next) {
   res.json({ msg: "This is CORS-enabled for all origins!" });
 });
 
 app.listen(3001, function () {
-  console.log("CORS-enabled web server listening on port 80");
+  console.log("CORS-enabled web server listening on port 3001");
 });
