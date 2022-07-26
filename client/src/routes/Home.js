f51c9f31fb8fe1d7f90664215ba6a6f05a5a9cf4
Jordan Larios <jolarios96@gmail.com>
2022-07-26 11:37:49 -0500

    placeholder routes

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,21 +1,20 @@
-import React from "react";
 import { Link } from "react-router-dom";
-import "./App.css";
-// import Home from "./routes/home";
 
-function App() {
+export default function Home() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hi there!</h1>
+        <h1>
+          This is the <b>"Home"</b> Page!
+        </h1>
         <p>First time?</p>
         <p>Here's some links</p>
         <Link to="/home">Home</Link>
+        <Link to="/PageNotFound">404 page</Link>
       </header>
 
       {/* <Route path="/home" exact render={(props) => <Home />} /> */}
     </div>
   );
 }
-
-export default App;
--- client/src/routes/PageNotFound.js
+++ client/src/routes/PageNotFound.js
@@ -1,21 +1,18 @@
-import React from "react";
 import { Link } from "react-router-dom";
-import "./App.css";
-// import Home from "./routes/home";
 
-function App() {
+export default function PageNotFound() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hi there!</h1>
+        <h1>
+          This is the <b>"404 Error"</b> Page!
+        </h1>
         <p>First time?</p>
         <p>Here's some links</p>
         <Link to="/home">Home</Link>
+        <Link to="/PageNotFound">404 page</Link>
       </header>
-
-      {/* <Route path="/home" exact render={(props) => <Home />} /> */}
     </div>
   );
 }
-
-export default App;
